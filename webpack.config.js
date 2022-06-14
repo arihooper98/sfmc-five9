48f91e4ec25b796304611b36adf7ce8e97d993e4
arihooper98 <ariana.hooper98@gmail.com>
2022-06-14 15:29:10 -0700

    changed listPriority

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const listPriority = require('./modules/list-priority/webpack.config');
 
 module.exports = function(env, argv) {
     return [
-        discountCodeExample(env, argv),
-        splitExample(env, argv),
+        listPriority(env, argv)
     ];
 };
